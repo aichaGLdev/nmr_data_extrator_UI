b7d2c45bf4d25bd80eaaccf55805da65a5970db1
Bayar Aicha <95301762+aichaGLdev@users.noreply.github.com>
2025-04-05 13:48:47 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8088/api/v1/auth',
   apiUrlV2: 'http://localhost:8088/api/v1/trail', // Update with your backend URL
-  apiUrlOneSolution :'http://51.195.216.43:8083/api/v1/matrices/generate?generationType=one',
-  apiUrlAllSolution :'http://51.195.216.43:8083/api/v1/matrices/generate'
+  apiUrlOneSolution :'',
+  apiUrlAllSolution :''
 
 };
